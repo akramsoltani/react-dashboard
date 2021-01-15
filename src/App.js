d5c89045b6ce04693bff683161c3d6640ea1541b
Akram <47157066+akramsoltani@users.noreply.github.com>
2021-01-15 19:54:30 +0100

    Created the sidebar and set up the basic routing functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'App.css';
+import ClippedDrawer from 'components/Drawer';
+import Notifications from 'pages/Notifications';
+import Homepage from 'pages/Homepage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' ;
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ClippedDrawer />
+      <Switch>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/notifications" component={Notifications} />
+      </Switch>
     </div>
-  );
+    </Router>
+  ); 
 }
 
 export default App;
